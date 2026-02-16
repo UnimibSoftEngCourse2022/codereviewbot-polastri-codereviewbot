22816c70120cb0f4a7a35ab67a9ed0872a301a12
LauraPolastri <l.polastri1@campus.unimib.it>
2026-02-16 16:01:25 +0100

    Quinto commit: creazione progetto maven e aggiunta packages di base

---
+++ codereviewbot/src/main/java/it/polastri/codereviewbot/App.java
@@ -0,0 +1,10 @@
+package it.polastri.codereviewbot;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
